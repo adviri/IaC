d13dcdfb5aafba9b9f1a593eaccadbd341aac3b9
adviri <ayalakr@gmail.com>
2022-12-22 12:07:04 +0200

    Update aws_s3_example.tf

--- aws/aws_s3_example.tf
+++ aws/aws_s3_example.tf
@@ -1,5 +1,5 @@
 # Server Side Encryption./....
-# new comment5
+# new comment5fasfa
 resource "aws_s3_bucket_server_side_encryption_configuration" "sse_example" {
   bucket = aws_s3_bucket.example.bucket
 
