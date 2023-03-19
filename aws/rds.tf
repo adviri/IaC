2288169bfb2bda70b0902a3ab5a51d5ab366f342
adviri <ayalakr@gmail.com>
2023-03-19 13:33:42 +0200

    Update aws/rds.tf
    Co-authored-by: jit-ci[bot] <91912817+jit-ci[bot]@users.noreply.github.com>

--- aws/rds.tf
+++ aws/rds.tf
@@ -1,4 +1,5 @@
 resource "aws_rds_cluster" "app11-rds-cluster" {
+  storage_encrypted = true
   cluster_identifier      = "app1-rds-cluster"
   allocated_storage       = 10
   backup_retention_period = 0
